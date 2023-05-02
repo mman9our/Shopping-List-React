f1de5a88e564b41fb03407fbfdd5a91fa9225cdc
mman9our <hack_225@outlook.sa>
2023-05-02 04:35:35 +0300

    Refactor the code in Routes secion add Lazy and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import CartContextProvider from "./context/CartContext";
 
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
 
 function App() {
   return (
-      <CartContextProvider>
-        <Navbar />
+    <CartContextProvider>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-      
-      </CartContextProvider>
+      </Suspense>
+    </CartContextProvider>
   );
 }
 
